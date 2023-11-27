8940f2e95f8e02f9284fba6b627873e72baeca32
pdecky1 <pdecky1@gmail.com>
2023-11-27 12:03:25 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 4px 50px 0px',
+      }
+    },
+  },
+  plugins: [],
+}
+
